64abf0181d592f3815e41076816a299434cd176b
Meheraj <57069206+MeherajUlMahmmud@users.noreply.github.com>
2022-09-22 21:58:50 +0600

    added weather model

--- weather_control/models.py
+++ weather_control/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class WeatherModel(models.Model):
+    weather_type = models.CharField(max_length=100)
+    min_temp = models.FloatField()
+    max_temp = models.FloatField()
+    is_active = models.BooleanField(default=True)
+    is_deleted = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.weather_type
+
+    class Meta:
+        verbose_name = "Weather Type"
+        verbose_name_plural = "Weather Types"

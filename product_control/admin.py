eeaaed44983e3a2024af6a04c6322648e581be74
Meheraj <57069206+MeherajUlMahmmud@users.noreply.github.com>
2022-09-22 23:29:11 +0600

    added product model

--- product_control/admin.py
+++ product_control/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import ProductTypeModel
+from .models import ProductTypeModel, ProductModel
 
 admin.site.register(ProductTypeModel)
+admin.site.register(ProductModel)
--- product_control/models.py
+++ product_control/models.py
@@ -14,3 +14,22 @@ class ProductTypeModel(models.Model):
     class Meta:
         verbose_name = 'Product Type'
         verbose_name_plural = 'Product Types'
+
+
+class ProductModel(models.Model):
+    product_type = models.ForeignKey(ProductTypeModel, on_delete=models.CASCADE)
+    product_name = models.CharField(max_length=50)
+    product_description = models.TextField(null=True, blank=True)
+    available_quantity = models.IntegerField(default=0)
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    is_active = models.BooleanField(default=True)
+    is_deleted = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.product_name
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'

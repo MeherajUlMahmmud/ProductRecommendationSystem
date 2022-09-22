5a42e75d7902a4d7addabe46ca667827436790bc
Meheraj <57069206+MeherajUlMahmmud@users.noreply.github.com>
2022-09-22 22:09:32 +0600

    added product type model

--- product_control/models.py
+++ product_control/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductTypeModel(models.Model):
+    product_type = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+    is_deleted = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.product_type
+
+    class Meta:
+        verbose_name = 'Product Type'
+        verbose_name_plural = 'Product Types'

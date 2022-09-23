8bea30d098c908655fbea33d4335d0afde90c4da
Meheraj <57069206+MeherajUlMahmmud@users.noreply.github.com>
2022-09-23 15:00:22 +0600

    updated product serializer

--- product_control/serializer.py
+++ product_control/serializer.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
 
 from product_control.models import ProductTypeModel, ProductModel
+from user_control.serializer import UserSerializer, VendorSerializer
 
 
-class ProductTypeSerializer(serializers.ModelSerializer):
+class ProductTypeAdminSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
 
     class Meta:
@@ -11,10 +12,28 @@ class ProductTypeSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class ProductSerializer(serializers.ModelSerializer):
+class ProductTypeUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductTypeModel
+        fields = ('id', 'product_type')
+
+
+class ProductAdminSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
-    product_type = ProductTypeSerializer()
+    product_type = ProductTypeAdminSerializer()
+    vendor = UserSerializer(read_only=True)
 
     class Meta:
         model = ProductModel
         fields = '__all__'
+
+
+class ProductUserSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only=True)
+    product_type = ProductTypeUserSerializer()
+    vendor = VendorSerializer(read_only=True)
+
+    class Meta:
+        model = ProductModel
+        fields = (
+            'id', 'product_name', 'product_description', 'product_type', 'vendor', 'unit_price', 'available_quantity',)
